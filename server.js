7c928ebff30dacba34107f1d4407e4f84d6bbc0f
Promise Johnson <chiemelapromise30@gmail.com>
2022-04-24 00:18:38 +0100

    Deployment

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(session({
     }),
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7,
-        domain: 'https://shopklassy.netlify.app',
+        domain: 'https://shopklassyapi.herokuapp.com',
         // httpOnly: true,
         secure: true,
         sameSite: "none"
